a3965778050df85b36c8ea11427fbcf46ba4514f
tinawong <tina.ty.wong@viewsonic.com>
2024-07-20 23:22:21 +0800

    feat: vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,38 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+/// <reference types="vite/client" />
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+import react from '@vitejs/plugin-react';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { defineConfig } from 'vite';
+import svgr from 'vite-plugin-svgr';
+
+const filename = fileURLToPath(import.meta.url);
+const _dirname = path.dirname(filename);
+
+export default defineConfig(() => {
+  return {
+    build: {
+      outDir: 'build',
+    },
+    plugins: [react(), svgr()],
+    define: {
+      _global: {},
+    },
+    resolve: {
+      alias: {
+        '@': path.resolve(_dirname, 'src'),
+      },
+    },
+    server: {
+      port: 3000,
+    },
+    test: {
+      globals: true,
+      environment: 'jsdom',
+      setupFiles: './src/test/setup.ts',
+      // you might want to disable it, if you don't have tests that rely on CSS
+      // since parsing CSS is slow
+      css: true,
+    },
+  };
+});
